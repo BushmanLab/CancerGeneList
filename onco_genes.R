26fe1048e0f763045fb179a54d868a42d3132a2c
Anatoly Dryga <anatolydryga@gmail.com>
2015-05-01 15:36:13 -0400

    read oncogenes frome files and compares to list of genes

--- onco_genes.R
+++ onco_genes.R
@@ -1,6 +1,11 @@
 
 #' check if gene is onco gene list(curated by Bushman's lab)
 #' @return TRUE if onco-gene FALSE if not
-is_onco_gene <- function(db_con, gene_symbol) {
+is_onco_gene <- function(gene_symbol_sites, oncogenes) {
+    toupper(gene_symbol_sites) %in% toupper(oncogenes)
+}
 
+get_oncogene_from_file <- function(filename) {
+    onco <- read.csv(filename, header=FALSE, stringsAsFactors=FALSE)
+    as.character(onco$V1)
 }
