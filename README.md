67ff3b67584003349b67d696b17043ee2a3100e5
Anatoly Dryga <anatolydryga@gmail.com>
2015-05-12 14:02:17 -0400

    gene list known from prev trials added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CancerGeneList
 maintain the list of cancer-associated genes  
+
+# Usage
+
+Currently list of genes of interest are stored in:
+
+* `allonco_no_pipes.csv` gene symbols of known onco genes
+* `genes_adverse_event.csv` list of genes that associated with adverse events in previous gene
+therapy trials
+
+
+
+```
+source('onco_genes.R')
+
+onco_genes <- get_oncogene_from_file('allonco_no_pipes.csv')
+adverse_genes <- get_adverse_event_genes('genes_adverse_event.csv')
+```
--- onco_genes.R
+++ onco_genes.R
@@ -1,11 +1,18 @@
-
 #' check if gene is onco gene list(curated by Bushman's lab)
 #' @return TRUE if onco-gene FALSE if not
 is_onco_gene <- function(gene_symbol_sites, oncogenes) {
     toupper(gene_symbol_sites) %in% toupper(oncogenes)
 }
 
+
+# @return vector of gene symbols
 get_oncogene_from_file <- function(filename) {
     onco <- read.csv(filename, header=FALSE, stringsAsFactors=FALSE)
     as.character(onco$V1)
+}
+
+# @return vector of gene symbols
+get_adverse_event_genes <- function(filename) {
+    adverse <- read.csv(filename, header=FALSE, stringsAsFactors=FALSE)
+    as.character(adverse$V1)
 }
