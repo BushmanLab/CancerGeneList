bf7277d621079a8f1e7e3c5e2d1654c75b4b51f9
John Everett <everett.jk@gmail.com>
2016-07-05 11:46:54 -0400

    Update onco_genes.R

--- onco_genes.R
+++ onco_genes.R
@@ -13,7 +13,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 #' check if gene is onco gene list(curated by Bushman's lab)
 #' @return TRUE if onco-gene FALSE if not
 is_onco_gene <- function(gene_symbol_sites, oncogenes) {
